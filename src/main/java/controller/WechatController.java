1e9892ff22ff04ea4d77aed99ca354472f0baa6d
Willow <revolyw@yeah.net>
2017-07-10 23:04:18 +0800

    微信服务器验证接口调整

--- src/main/java/controller/WechatController.java
+++ src/main/java/controller/WechatController.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import service.WechatService;
+import util.LoggerUtil;
 
 /**
  * 微信服务接口
@@ -15,11 +16,11 @@ import service.WechatService;
 public class WechatController {
     @RequestMapping("")
     public String init(HTTP http) {
-        String echoStr = http.getString("echoStr", "");
+        String echoStr = http.getString("echostr", "");
         String signature = http.getString("signature", "");
         String timestamp = http.getString("timestamp", "");
         String nonce = http.getString("nonce", "");
-
+        LoggerUtil.info("echoStr");
         boolean validResult = WechatService.validateSignature(signature, timestamp, nonce);
         return validResult ? echoStr : "fail";
     }
